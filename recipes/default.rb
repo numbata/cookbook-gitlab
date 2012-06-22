aeba4dc055345c411c2870a5d448322b1ed02dc3
Andreas Simon <mail@andreas-simon.info>
2012-06-22 11:41:19 +0200

    Separate the installation of Gitlab from serving the app via nginx
    Allows other configuration options, e.g. Apache and Passenger

--- recipes/default.rb
+++ recipes/default.rb
@@ -190,36 +190,3 @@ execute "gitlab-bundle-rake" do
   group node['gitlab']['group']
   not_if { File.exists?("#{node['gitlab']['app_home']}/db/production.sqlite3") }
 end
-
-# Render unicorn template
-template "#{node['gitlab']['app_home']}/config/unicorn.rb" do
-  owner node['gitlab']['user']
-  group node['gitlab']['group']
-  mode 0644
-end
-
-package 'daemon'
-
-# Render unicorn_rails init script
-template "/etc/init.d/unicorn_rails" do
-  owner "root"
-  group "root"
-  mode 0755
-  source "unicorn_rails.init.erb"
-end
-
-# Start unicorn_rails and nginx service
-%w{ unicorn_rails nginx }.each do |svc|
-  service svc do
-    action [ :start, :enable ]
-  end
-end
-
-# Render nginx default vhost config
-template "/etc/nginx/conf.d/default.conf" do
-  owner "root"
-  group "root"
-  mode 0644
-  source "nginx.default.conf.erb"
-  notifies :restart, "service[nginx]"
-end
